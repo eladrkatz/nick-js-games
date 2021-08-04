da79a6390792f1bdad2e35683bb9755f12e5ecf8
Nick Zhirov <nickosman10@gmail.com>
2021-08-04 21:24:10 +0300

    finish chessboard

--- canvas.js
+++ canvas.js
@@ -8,40 +8,25 @@ function window_loaded() {
 
     const ctx = theCanvas.getContext('2d');
     
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(20, 10, 50, 50);
-
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
-
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(300, 150);
-    ctx.lineTo(300, 250);
-    ctx.stroke();
-
-
-    for(let i=0; i<10; i++) {
+    for(let j = 0; j < 9; j++){
         ctx.beginPath();
-        ctx.moveTo(i * 10, 0);
-        ctx.lineTo(i * 10, 150);
+        ctx.moveTo(100.5, 100.5 + j * 100);
+        ctx.lineTo(900.5, 100.5 + j * 100);
+        ctx.moveTo(100.5  + j * 100, 100.5);
+        ctx.lineTo(100.5 + j * 100, 900.5);
         ctx.stroke();
     }
-
-    ctx.beginPath();
-    ctx.moveTo(500, 500);
-    ctx.lineTo(500, 580);
-    ctx.lineTo(580, 580);
-    ctx.lineTo(580, 500);
-    ctx.lineTo(500, 500);
-    ctx.stroke();
-
-    for(let j = 1; j < 8; j++){
-        ctx.beginPath();
-        ctx.moveTo(500, 500 + j * 10);
-        ctx.lineTo(580, 500 + j * 10);
-        ctx.moveTo(500  + j * 10, 500);
-        ctx.lineTo(500 + j * 10, 580);
-        ctx.stroke();
+    
+    for(let i1 = 1; i1 < 9; i1++){
+        for(let i2 = 1; i2 < 9; i2++){
+            if(i1 % 2 == 0){
+                ctx.fillStyle = 'rgb(255, 255, 0)';
+                ctx.fillRect(i2 * 200 + 0.5, i1 * 100 + 0.5, 100, 100);
+            }
+            else if(i2 % 2 != 0){
+                ctx.fillStyle = 'rgb(255, 255, 0)';
+                ctx.fillRect(i2 * 100 + 0.5, i1 * 100 + 0.5, 100, 100);
+            }
+        }
     }
 }
